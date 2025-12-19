41b33981f8cfcc08595ab27f54984eca128bbb33
sharon17kevin <sharon17kevin@gmail.com>
2025-12-19 21:19:14 +0100

    Create the sellers orders page

---
+++ mobile-app/app/(tabs)/(sell)/_layout.tsx
@@ -0,0 +1,8 @@
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+  return <Stack initialRouteName='index'>
+    <Stack.Screen name="index" options={{ headerShown: false }} />
+    <Stack.Screen name="orders" options={{ headerShown: false }} />
+  </Stack>
+}
